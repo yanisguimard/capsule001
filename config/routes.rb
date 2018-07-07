0989fd7872ea7d7eade360c7d7574a1f93614f16
yanisguimard <yanisguim@gmail.com>
2018-07-07 19:08:23 +0200

    Link title to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     get 'code_snippets/index'
     
+    get 'index' => "code_snippets#index"
     get 'code_display' => "code_snippets#code_display"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'code_snippets#index'
