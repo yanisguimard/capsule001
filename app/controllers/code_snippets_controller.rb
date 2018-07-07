a6d43a1e5f9f12720f7b0e1d56f2cbffac235cca
yanisguimard <yanisguim@gmail.com>
2018-07-07 14:39:18 +0200

    Adjust header's color according to language displayed

--- app/controllers/code_snippets_controller.rb
+++ app/controllers/code_snippets_controller.rb
@@ -8,5 +8,24 @@ class CodeSnippetsController < ApplicationController
         @declaration_snippets = CodeSnippet.where(language: @language, category: "declaration")
         @condition_snippets = CodeSnippet.where(language: @language, category: "condition")
         @loop_snippets = CodeSnippet.where(language: @language, category: "loop")
+        color_display
     end
+
+    def color_display
+      case @language
+        when "c"
+          @color = "#1a8cff"
+        when "cpp"
+          @color = "#66c2ff"
+        when "java"
+          @color = "#ffad33"
+        when "javascript"
+          @color = "#ffff4d"
+        when "python"
+          @color = "#2eb82e"
+        when "ruby"
+          @color = "#ff3333"
+      end
+    end
+
 end
